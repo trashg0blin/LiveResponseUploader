74b060d1ee9246a2ea4447b20b23030db054a4c8
Christopher Smith <34755597+trashg0blin@users.noreply.github.com>
2023-08-18 22:16:14 -0400

    Update __init__.py

--- LiveResponsePipeline/__init__.py
+++ LiveResponsePipeline/__init__.py
@@ -2,6 +2,4 @@ import logging
 from os import environ
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-
-
 logging.basicConfig(filename=environ["LOG_NAME"], encoding='utf-8',level=environ["LOG_LEVEL"])
